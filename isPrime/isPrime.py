6a773341d68c0677f4e4ca6ceadab70c2a8f2312
Jesse Darr <jesse.darr@ibm.com>
2020-04-23 12:19:09 -0500

    adding isPrime

---
+++ isPrime/isPrime.py
@@ -0,0 +1,27 @@
+import math
+
+def isPrime(num):
+    if num == 2:
+        return True
+    elif num % 2 == 0 or num < 2: 
+        return False
+    else:
+        i = math.floor(math.sqrt(num)) # use floor b/c decimal will never return a clean sqrt
+        while (i >= 3):
+            if num % i == 0:
+                return False
+            i -= 1
+        return True        
+
+
+print(f'2:   {isPrime(2)}')
+print(f'3:   {isPrime(3)}')
+print(f'5:   {isPrime(5)}')
+print(f'7:   {isPrime(7)}')
+print(f'9:   {isPrime(9)}')
+print(f'11:  {isPrime(11)}')
+print(f'99:  {isPrime(99)}')
+print(f'100: {isPrime(100)}')
+print(f'101: {isPrime(101)}')
+print(f'102: {isPrime(102)}')
+print(f'103: {isPrime(103)}')
